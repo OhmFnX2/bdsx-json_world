0d83559eb3af90fff96aa5458017e946798c812f
OhmFn X2 <64475671+OhmFnX2@users.noreply.github.com>
2022-06-03 04:49:40 +0700

    v1.0.5 fix command & fix save for loop

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
-import { BlockPos, Vec3 } from "bdsx/bds/blockpos"
-import { Player } from "bdsx/bds/player"
 import { events } from "bdsx/event"
 import { bedrockServer } from "bdsx/launcher"
+import * as fs from "fs"
+import * as path from "path"
 
 events.serverOpen.on(() => {
     console.log("[Json World] launching");
+    if (!fs.existsSync(path.join(__dirname, "./world"))) return fs.mkdirSync(path.join(__dirname, "./world"))
 });
 events.serverClose.on(() => {
     console.log("[Json World] stoping");
--- src/main.ts
+++ src/main.ts
@@ -1,70 +1,32 @@
 import { Block } from "bdsx/bds/block"
 import { BlockPos, Vec3 } from "bdsx/bds/blockpos"
-import { CommandPermissionLevel, CommandStringEnum } from "bdsx/bds/command"
+import { CommandPermissionLevel, CommandPosition } from "bdsx/bds/command"
 import { TextPacket } from "bdsx/bds/packets"
 import { Player } from "bdsx/bds/player"
 import { command } from "bdsx/command"
-import { events } from "bdsx/event"
 import { bool_t, CxxString } from "bdsx/nativetype"
 import * as fs from "fs"
 import * as path from "path"
-
-let pos1: Map<Player, Vec3> = new Map<Player, Vec3>()
-
-let pos2: Map<Player, Vec3> = new Map<Player, Vec3>()
-
-const range = (start: number, end: number) => Array.from({length: (end - start)}, (v, k) => k + start);
-
-events.serverOpen.on(() => {
-    if (!fs.existsSync(path.join(__dirname, "../world"))) return fs.mkdirSync(path.join(__dirname, "../world"))
-})
 
 let cmd = command.register("jsonworld", "Convert World To JsonWorld", CommandPermissionLevel.Operator)
 
 cmd.overload((p, o) => {
     const player = o.getEntity() as Player
     if (!(player instanceof Player)) return false
-    pos1.set(player, player.getFeetPos())
-    sendMessage(player, "Set pos1")
-    return true
-}, {
-    mode: command.enum("jsonworld.pos1", "pos1")
-})
-
-cmd.overload((p, o) => {
-    const player = o.getEntity() as Player
-    if (!(player instanceof Player)) return false
-    pos2.set(player, player.getFeetPos())
-    sendMessage(player, "Set pos2")
-}, {
-    mode: command.enum("jsonworld.pos2", "pos2")
-})
-
-cmd.overload((p, o) => {
-    const player = o.getEntity() as Player
-    if (!(player instanceof Player)) return false
     let ph = path.join(__dirname, `../world/${p.filename}.json`)
-    if (pos1.get(player) == null) {
-        sendMessage(player, "pos1 is null")
-        return false
-    }
-    if (pos2.get(player) == null) {
-        sendMessage(player, "pos2 is null")
-        return false
-    }
     fs.writeFileSync(ph, "")
     fs.appendFileSync(ph, `{"json_world":[`)
-    let poss1 = pos1.get(player)
-    let poss2 = pos2.get(player)
-    let x1 = Math.floor(Math.min(poss1?.x as number, poss2?.x as number))
-    let x2 = Math.floor(Math.max(poss1?.x as number, poss2?.x as number))
-    let y1 = Math.floor(Math.min(poss1?.y as number, poss2?.y as number))
-    let y2 = Math.floor(Math.max(poss1?.y as number, poss2?.y as number))
-    let z1 = Math.floor(Math.min(poss1?.z as number, poss2?.z as number))
-    let z2 = Math.floor(Math.max(poss1?.z as number, poss2?.z as number))
-    for(let x of range(x1, x2)) {
-        for(let y of range(y1, y2)) {
-            for(let z of range(z1, z2)) {
+    let x1 = Math.floor(Math.min(p.pos1.x, p.pos2.x))
+    let x2 = Math.floor(Math.max(p.pos1.x, p.pos2.x))
+    let y1 = Math.floor(Math.min(p.pos1.y, p.pos2.y))
+    let y2 = Math.floor(Math.max(p.pos1.y, p.pos2.y))
+    let z1 = Math.floor(Math.min(p.pos1.z, p.pos2.z))
+    let z2 = Math.floor(Math.max(p.pos1.z, p.pos2.z))
+    p.save_air = false
+    console.log(p.save_air)
+    for(let x = x1; x <= x2; x++) {
+        for(let y = y1; y <= y2; y++) {
+            for(let z = z1; z <= z2; z++) {
                 let block = player.getDimension().blockSource.getBlock(BlockPos.create(x, y, z))
                 let blockId = block.blockLegacy.getCommandName()
                 if (blockId === "minecraft:air" && p.save_air === false) continue
@@ -80,6 +42,8 @@ cmd.overload((p, o) => {
     return true
 }, {
     mode: command.enum("jsonworld.save", "save"),
+    pos1: [CommandPosition, false],
+    pos2: [CommandPosition, false],
     filename: [CxxString, false],
     save_air: [bool_t, true]
 })
@@ -94,6 +58,7 @@ cmd.overload((p, o) => {
     let data = fs.readFileSync(ph)
     let json = JSON.parse(data.toString())
     let blocks = json.json_world
+    p.load_air = false
     for (let block of blocks) {
         let objBlock = block
         let blockId = objBlock.block_id
@@ -103,6 +68,7 @@ cmd.overload((p, o) => {
         let y = pos.y
         let z = pos.z
         if (y>=500) continue
+        if (blockId === "minecraft:air" && p.load_air === false) continue
         try {
             player.getDimension().blockSource.setBlock(BlockPos.create(x, y, z), Block.create(blockId, blockData) as Block)
         } catch (e) {
